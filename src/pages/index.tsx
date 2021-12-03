93e24b07cca498df25b75c862221e2bd64ca85bc
christopher-besch <christopher.besch@gmx.de>
2021-12-03 16:32:03 +0100

    broken scaling mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,57 @@ import * as styles from "../styles/home.module.css";
 const Home: React.FC = () => {
     return (
         <Layout>
-            <div className={styles.portfolio}>
+            <div className={styles.portfolio_heading}>
                 <p>Christopher Besch</p>
                 <hr />
                 <h2>Portfolio</h2>
                 <p>Little info</p>
             </div>
-        </Layout>
+            <div className={styles.row}>
+                <div className={styles.column}>
+                    <div className={styles.content}>
+                        <img src="https://www.w3schools.com/w3images/lights.jpg" alt="broken image" style={{ width: "100%" }} />
+                        <h3>My Work</h3>
+                        <p>I like cheese</p>
+                    </div>
+                </div>
+                <div className={styles.column}>
+                    <div className={styles.content}>
+                        <img src="https://www.w3schools.com/w3images/lights.jpg" alt="broken image" style={{ width: "100%" }} />
+                        <h3>My Work</h3>
+                        <p>I like cheese</p>
+                    </div>
+                </div>
+                <div className={styles.column}>
+                    <div className={styles.content}>
+                        <img src="https://www.w3schools.com/w3images/lights.jpg" alt="broken image" style={{ width: "100%" }} />
+                        <h3>My Work</h3>
+                        <p>I like cheese</p>
+                    </div>
+                </div>
+                <div className={styles.column}>
+                    <div className={styles.content}>
+                        <img src="https://www.w3schools.com/w3images/lights.jpg" alt="broken image" style={{ width: "100%" }} />
+                        <h3>My Work</h3>
+                        <p>I like cheese</p>
+                    </div>
+                </div>
+                <div className={styles.column}>
+                    <div className={styles.content}>
+                        <img src="https://www.w3schools.com/w3images/lights.jpg" alt="broken image" style={{ width: "100%" }} />
+                        <h3>My Work</h3>
+                        <p>I like cheese</p>
+                    </div>
+                </div>
+                <div className={styles.column}>
+                    <div className={styles.content}>
+                        <img src="https://www.w3schools.com/w3images/lights.jpg" alt="broken image" style={{ width: "100%" }} />
+                        <h3>My Work</h3>
+                        <p>I like cheese</p>
+                    </div>
+                </div>
+            </div>
+        </Layout >
     )
 };
 export default Home;
