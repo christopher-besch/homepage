d214e6c7a9c69dcc3ab81977354731a51a3fb540
christopher-besch <christopher.besch@gmx.de>
2021-12-03 07:26:50 +0100

    type check broken

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Layout from "../components/layout";
+import * as styles from "../styles/home.module.css";
 
 const Home: React.FC = () => {
     return (
         <Layout>
-            <p>Hello World</p>
-        </ Layout>
+            <div className={styles.portfolio}>
+                <p>Christopher Besch</p>
+                <hr />
+                <h2>Portfolio</h2>
+                <p>Little info</p>
+            </div>
+        </Layout>
     )
 };
 export default Home;
