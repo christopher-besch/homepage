1714c86793cfedcb930257ac34f6cf4197c0f0d3
christopher-besch <christopher.besch@gmx.de>
2021-12-05 06:49:35 +0100

    removed fontawesome

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,9 +61,6 @@ module.exports = {
                 ]
             }
         },
-        {
-            resolve: "gatsby-plugin-fontawesome-css",
-        },
     ],
     // can be loaded from page with GraphQL
     siteMetadata: {
