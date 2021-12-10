8beaccc151fb571f477a692b3b762b6eeaa55fac
christopher-besch <christopher.besch@gmx.de>
2021-12-10 16:51:56 +0100

    fixed icons on chrome...again

--- src/utils/svg_mask.tsx
+++ src/utils/svg_mask.tsx
@@ -1,6 +1,6 @@
 export default function get_mask(url: string): object {
     return ({
         maskImage: `url("${url}")`,
-        webkitMaskImage: `url("${url}")`,
+        WebkitMaskImage: `url("${url}")`,
     });
 };
