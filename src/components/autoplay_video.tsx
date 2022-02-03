6a59481285050f67f8c68ed6673a5412543af5c4
christopher-besch <christopher.besch@gmx.de>
2022-02-03 19:05:53 +0100

    fixed video height

--- src/components/autoplay_video.tsx
+++ src/components/autoplay_video.tsx
@@ -6,13 +6,19 @@ interface AutoPlayVideoProps {
     src: string;
     poster?: string;
     width?: number;
+    height?: number;
 }
 const AutoPlayVideo: React.FC<AutoPlayVideoProps> = (props) => {
     return (
         <div>
             {props.poster ? undefined : <p className={styles.removed_warn}>[video removed from print]</p>}
             {/* show in print version if poster defined */}
-            <video className={props.poster ? styles.video : styles.video_no_print} controls loop muted autoPlay poster={props.poster} width={props.width ? props.width : 1920}>
+            <video
+                controls loop muted autoPlay
+                className={props.poster ? styles.video : styles.video_no_print}
+                poster={props.poster}
+                width={props.width ? props.width : 1920}
+                height={props.height ? props.height : 1080}>
                 <source src={props.src} type="video/mp4" />
             </video>
         </div>
