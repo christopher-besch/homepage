b0fc5b4bb52d7c08842f7395f0fcd54561775c7d
christopher-besch <mail@chris-besch.com>
2022-10-01 18:59:54 +0200

    garbage?

--- src/components/iframe.tsx
+++ src/components/iframe.tsx
@@ -13,19 +13,22 @@ interface IframeProps {
     fullscreen?: boolean;
 }
 const Iframe: React.FC<IframeProps> = (props) => {
-    const data: IframeData = useStaticQuery(graphql`
-query IframeData {
-  site {
-    siteMetadata {
-      present_url
-    }
-  }
-}
-    `);
+//     const data: IframeData = useStaticQuery(graphql`
+// query IframeData {
+//   site {
+//     siteMetadata {
+//       present_url
+//     }
+//   }
+// }
+//     `);
     // xor
     if ((props.src == undefined) == (props.present == undefined))
         throw new Error("Iframe needs src or present parameter");
-    let src = props.present ? `${data.site?.siteMetadata?.present_url}/${props.present}` : props.src;
+    // TODO: check type
+    // const present_url = data.site?.siteMetadata?.present_url;
+    const present_url = "https://example.com";
+    const src = props.present ? `${data.site?.siteMetadata?.present_url}/${props.present}` : props.src;
     return (
         <HalfElement full={true}>
             <div className={styles.wrapper}>
