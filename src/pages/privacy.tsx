3931488d5afe3e7c92b01941ff524f013e491d01
christopher-besch <mail@chris-besch.com>
2022-10-12 17:14:35 +0200

    fixes #90

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -17,15 +17,34 @@ const Privacy = ({ data }: PageProps<Queries.PrivacyQuery>) => {
                     This privacy policy will explain how I and any necessary third-party services use the personal data collected from you when you use my website.
                 </p>
 
-                <h1>tl;dr</h1>
-                <p>
-                    This website collects your IP address; you should read <a href="https://www.netlify.com/gdpr-ccpa">Netlify's privacy policy</a>.
-                </p>
-
                 <h1>What data do I collect?</h1>
                 <p>
-                    I myself don't collect any personal data.
-                    Though my hoster <a href="https://www.netlify.com">Netlify</a> does keep an access log required to operate this website.
+                    This website uses <a href="https://www.cloudflare.com/en-gb/web-analytics">Cloudflare Web Analytics</a>, which collects the following data:
+                    <ul>
+                        <li>time of access,</li>
+                        <li>what page you read,</li>
+                        <li>your operating system (Linux, Windows, MacOS, Android, iOS, ...),</li>
+                        <li>your browser,</li>
+                        <li>if you're using a mobile device,</li>
+                        <li>the referer (i.e. the page that linked you to my homepage),</li>
+                        <li>from what country you're accesing my homepage and</li>
+                        <li>some website diagnostics, like how long my homepage takes to load for you.</li>
+                    </ul>
+                </p>
+                <p>
+                    This data is anonymized.
+                    So I for example only know that ten people from Belgium visited my <Link to="/articles/revealjs">article about reveal.js</Link> this month—not that <b>you</b> visited my page.
+                </p>
+                <p>
+                    This data is only accessible by Cloudflare (for six months) and myself.
+                    Short overviews of the data are kept indefinitely by me and I might publish them in the future.
+                </p>
+                <p>
+                    This data is used to help me understand what topics people are interested in.
+                </p>
+
+                <p>
+                    In addition to that, my hoster <a href="https://www.netlify.com">Netlify</a> keeps an access log required to operate this website.
                     This access log includes most notably your IP address and access time stamps, which are stored (by Netlify) for less than 30 days.
                 </p>
                 <p>
@@ -50,15 +69,13 @@ const Privacy = ({ data }: PageProps<Queries.PrivacyQuery>) => {
                 <h1>Changes to my privacy policy</h1>
                 <p>
                     I keep my privacy policy under regular review and place any updates on this web page.
-                    This privacy policy was last updated on 2 January 2022.
+                    This privacy policy was last updated on 12th October 2022.
                 </p>
 
                 <h1>How to contact me</h1>
-
                 <p>
                     If you have any questions about my privacy policy, please do not hesitate to <Link to="/about">contact me</Link>.
                 </p>
-
                 <p>
                     Email me at: <a href={email_link}>{email_name}</a>
                 </p>
@@ -85,3 +102,4 @@ query Privacy {
 export const Head = () => (
     <SEO heading="Privacy Policy" />
 );
+
