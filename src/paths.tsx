bd5147dde11cb69b8ea7245e88fc9d440f71e2a7
christopher-besch <mail@chris-besch.com>
2025-12-02 17:16:23 +0100

    fix: race condition with fs.cp

--- src/paths.tsx
+++ src/paths.tsx
@@ -54,9 +54,11 @@ export function createImageDeployPath(hash: string, width: number, height: numbe
 // static //
 const deployFontsPath = path.join(deployStylesPath, `fonts`);
 export function copyStatic() {
-    console.log(fs.readdirSync(deployPath));
-    fs.cp(staticPath, deployPath, { recursive: true, force: true, errorOnExist: false }, (err) => { if (err != null) { throw err; } });
-    fs.cp("./node_modules/katex/dist/fonts", deployFontsPath, { recursive: true, force: true, errorOnExist: false }, (err) => { if (err != null) { throw err; } });
+    // Make sure we execute one after the other.
+    // Because of this the static dir may only contain fonts and things that aren't in subdirs that other code also accesses.
+    fs.promises.cp(staticPath, deployPath, { recursive: true }).then(() => {
+        fs.promises.cp("./node_modules/katex/dist/fonts", deployFontsPath, { recursive: true });
+    });
 }
 
 // articles //
