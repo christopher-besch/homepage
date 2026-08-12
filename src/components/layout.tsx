14d86866b04865b75e45b8ee3fe9c95e7f3b879c
christopher-besch <mail@chris-besch.com>
2026-08-12 13:05:53 +0200

    feat: add DC8RIS

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -147,7 +147,7 @@ export default async function Layout(props: React.PropsWithChildren<LayoutProps>
                     <Link className="layout_navbar_left" href="/">
                         <div>
                             <h1>Christopher&nbsp;Besch</h1>
-                            <h2>Developer&bull;Photographer</h2>
+                            <h2>Developer&bull;Photographer&bull;DC8RIS</h2>
                         </div>
                         <Image lazy={false} transparent={true} inputPath={getResourceLoadPath("snake.png")} />
                     </Link>
